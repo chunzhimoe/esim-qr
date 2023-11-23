42b3e844f88b21613fef1216cfc57c3fb41e235f
chunzhi <60135925+chunzhimoe@users.noreply.github.com>
2023-11-23 18:58:20 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import qrcode
+from PIL import Image
 
 # 设置请求头
 headers = {
@@ -14,14 +16,13 @@ headers = {
 
 # 设置请求数据
 data = {
-    'password': '',
-    'phoneNumber': ''
+    'password': 'xxxxx',
+    'phoneNumber': '06xxxxxx'
 }
 
 # 发送POST请求获取sessionToken
 response = requests.post('https://appapi.simyo.nl/simyoapi/api/v1/sessions', headers=headers, data=json.dumps(data))
 response_json = response.json()
-print(response_json)  # 调试代码，打印API返回的JSON
 sessionToken = response_json.get('result').get('sessionToken')
 
 # 设置请求头，加入sessionToken
@@ -30,5 +31,40 @@ headers['X-Session-Token'] = sessionToken
 # 发送GET请求获取activationCode
 response = requests.get('https://appapi.simyo.nl/simyoapi/api/v1/esim/get-by-customer', headers=headers)
 response_json = response.json()
-print(response_json)  # 打印API返回的JSON
+activationCode = response_json.get('result').get('activationCode')
 
+# 添加"LPA:"前缀
+activationCode = 'LPA:' + activationCode
+
+# 生成二维码
+qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+qr.add_data(activationCode)
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+
+# 调整二维码大小
+img.thumbnail((250, 250))
+
+# 将二维码转换为字符画
+width, height = img.size
+scale = 1
+if width > 80:
+    scale = width // 80 + 1
+    width //= scale
+    height //= scale
+img = img.resize((width, height))
+pixels = img.load()
+output = ""
+for y in range(height):
+    for x in range(width):
+        output += " " if pixels[x, y] == 255 else "█"
+    output += "\n"
+
+# 输出结果
+print('sessionToken:', sessionToken)
+print('activationCode:', activationCode)
+print('二维码已生成：')
+print(output)
+
+# 显示二维码
+img.show()
